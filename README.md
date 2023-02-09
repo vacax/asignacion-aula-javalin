68263b7a4a03dbb96de86482b92c079ca9c8a259
Carlos Camacho <ca.camacho@ce.pucmm.edu.do>
2023-02-08 23:43:08 -0400

    Commit de inicio.

---
+++ README.md
@@ -0,0 +1,18 @@
+# Proyecto de asignación en aula
+
+Implementa un filtro para todas las peticiones, 
+de no encontrar un usuario en sesión, reenvia a un formulario 
+para el login, una vez autenticado permitie acceder al recurso de la barra.
+
+## Ejecutar:
+
+```
+./gradlew run
+```
+Acceder al enlace https://localhost:7070/
+
+## Librerías y plataformas utilizadas:
+* Javalin 5.3.2
+* Slf4j-simple 2.0.3
+* J2html 1.6.0
+* Java 17
---
+++ src/main/java/edu/pucmm/eict/Main.java
@@ -0,0 +1,119 @@
+package edu.pucmm.eict;
+
+import io.javalin.Javalin;
+import io.javalin.http.ContentType;
+import io.javalin.http.staticfiles.Location;
+import static j2html.TagCreator.*;
+
+/**
+ * Ejemplo de asignación en aula.
+ */
+public class Main {
+
+    public enum Constantes{
+        USUARIO_LOGUEADO;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Ejemplo de asignación en aula");
+        /**
+         *
+         */
+        var app = Javalin.create(javalinConfig -> {
+                    javalinConfig.staticFiles.add(staticFileConfig -> {
+                        staticFileConfig.hostedPath = "/";
+                        staticFileConfig.directory = "/publico";
+                        staticFileConfig.location = Location.CLASSPATH;
+                    });
+                })
+                .start(7070);
+
+        /**
+         * Trabajando con el filtro, cualquier acceso sin el usuario auntenticado, debe enviarlo al recurso
+         * login.html
+         */
+        app.before(ctx -> {
+            //validando si existe el usuario logueado.
+            System.out.println("Logueado: "+Constantes.USUARIO_LOGUEADO.name());
+            Usuario usuario = ctx.sessionAttribute(Constantes.USUARIO_LOGUEADO.name());
+            /**
+             * Si, no
+             */
+            if(usuario== null && !(ctx.path().contains("login.html") || ctx.path().contains("/autenticar"))){
+                ctx.redirect("/login.html");
+            }
+        });
+
+        app.get("/", ctx -> {
+            Usuario usuario = ctx.sessionAttribute(Constantes.USUARIO_LOGUEADO.name());
+            //
+            String texto = html(
+                    head(
+                            title("Asignacion en Aula")
+                    ),
+                    body(
+                            h1("Bienvenido "+usuario.getUsername()),
+                            p("Ejemplo de uso de la libreria j2html"),
+                            a("Salir").withHref("/logout"))
+            )
+                    .render();
+            //
+            ctx.contentType(ContentType.TEXT_HTML);
+            ctx.result(texto);
+        });
+
+        /**
+         * Para nuestro ejemplo no importa los valores recibido, lo estaremos validando.
+         */
+        app.post("/autenticar", ctx -> {
+            //
+            String username = ctx.formParam("username");
+            String password = ctx.formParam("password");
+            //
+            Usuario usuario = new Usuario(username, password);
+            ctx.sessionAttribute(Constantes.USUARIO_LOGUEADO.name(), usuario);
+            //
+            ctx.redirect("/");
+        });
+
+        /**
+         * Proceso de salida de la sesión
+         */
+        app.get("/logout", ctx -> {
+            ctx.req().getSession().invalidate();
+            ctx.redirect("/");
+        });
+    }
+
+    /**
+     * Clase de encapsulación, puede ser un record
+     */
+    public static class Usuario{
+        String username;
+        String password;
+
+        public Usuario() {
+        }
+
+        public Usuario(String username, String password) {
+            this.username = username;
+            this.password = password;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
+}
